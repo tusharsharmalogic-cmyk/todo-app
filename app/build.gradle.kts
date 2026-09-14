d5dec43babbf4f1fbebf6f88b7245dd4d3b1ba4a
User <user@example.com>
2026-09-14 21:55:51 +0530

    Bump AGP 8.1.4, compileSdk 34, Kotlin 1.9.10, compose compiler 1.5.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.helloworld"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.helloworld"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 2
         versionName = "2.0"
 
@@ -41,7 +41,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 
     packaging {
